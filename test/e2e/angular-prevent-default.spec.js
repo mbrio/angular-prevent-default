1cf68faf371654f5fff296fde97ef82ab7162b41
Michael Diolosa <michael.diolosa@gmail.com>
2013-03-28 14:08:21 -0400

    Update pages

--- test/e2e/angular-prevent-default.spec.js
+++
@@ -1,7 +0,0 @@
-describe('prevent-default', function () {
-  it('should prevent a link\'s default action', function () {
-    browser().navigateTo('/');
-    element('a').click();
-    expect(browser().window().hash()).toBe('');
-  });
-});
